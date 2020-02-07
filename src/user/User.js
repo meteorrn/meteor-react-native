f7fe2c248e7666a1f75e3d6f172b2883cc03f47b
Nathaniel Dsouza <nate@kidzideaz.xyz>
2020-02-06 19:44:10 -0500

    Update User.js

--- src/user/User.js
+++ src/user/User.js
@@ -1,4 +1,4 @@
-import AsyncStorage from '@react-native-community/async-storage';
+import { AsyncStorage } from 'react-native';
 
 import Data from '../Data';
 import { hashPassword } from '../../lib/utils';
