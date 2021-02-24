902e1e44e22de0bfea0c4fc2e931283fcd457a99
Nathaniel Dsouza <nate@kidzideaz.xyz>
2021-02-24 11:50:00 -0500

    Update Data.js

--- src/Data.js
+++ src/Data.js
@@ -14,23 +14,12 @@ const moduleExists = m => {
   return true;
 };
 
-let afterInteractions;
-if(moduleExists("react-native")) { 
-  db.batchedUpdates = require('react-native/Libraries/Renderer/shims/ReactNative').unstable_batchedUpdates;
-  process.nextTick = setImmediate;
-  afterInteractions = require('react-native').InteractionManager.runAfterInteractions;
+let afterInteractions = requestIdleCallback;  
+try {
+  db.batchedUpdates = require('react-dom').unstable_batchedUpdates;
 }
-else {
-  // This code allows the package to work on the web, but it causes an issue in Metro <0.59.0, which is hard to upgrade to due to the react native community CLI not supporting it
-  /*
-  afterInteractions = requestIdleCallback;  
-  try {
-    db.batchedUpdates = require('react-dom').unstable_batchedUpdates;
-  }
-  catch(e) {
-    console.warn("react-dom isn't installed, so minimongo-cache performance may be reduced");
-  }
-  */
+catch(e) {
+  console.warn("react-dom isn't installed, so minimongo-cache performance may be reduced");
 }
 
 function runAfterOtherComputations(fn) {
