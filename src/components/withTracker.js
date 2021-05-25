a63ce64c934869407423bfbb057b28384a14fe3c
Zach Oligschlaeger <zach@make.toys>
2021-05-25 13:01:47 -0700

    fix

--- src/components/withTracker.js
+++ src/components/withTracker.js
@@ -1,29 +1,16 @@
-
 import React, { forwardRef, memo } from 'react';
 import useTracker from './useTracker';
 
-type ReactiveFn = (props: object) => any;
-type ReactiveOptions = {
-  getMeteorData: ReactiveFn;
-  pure?: boolean;
-  skipUpdate?: (prev: any, next: any) => boolean;
-}
+export default function withTracker (options) {
+    return Component => {
+        const expandedOptions = typeof options === 'function' ? { getMeteorData: options } : options;
+        const { getMeteorData, pure = true } = expandedOptions;
 
-export default function withTracker(options: ReactiveFn | ReactiveOptions) {
-  return (Component) => {
-    const expandedOptions = typeof options === 'function' ? { getMeteorData: options } : options;
-    const { getMeteorData, pure = true } = expandedOptions;
-    const WithTracker = forwardRef((props, ref) => {
-      const data = useTracker(
-        () => {return getMeteorData(props) || {}},
-        [options, props]
-      );
-      
-      return (
-        <Component ref={ref} {...props} {...data} />
-      );
-    });
+        const WithTracker = forwardRef((props, ref) => {
+            const data = useTracker(() => getMeteorData(props) || {}, [props]);
+            return React.createElement(Component, {ref, ...props, ...data});
+        });
 
-    return pure ? memo(WithTracker) : WithTracker;
-  };
+        return pure ? memo(WithTracker) : WithTracker;
+    };
 }
