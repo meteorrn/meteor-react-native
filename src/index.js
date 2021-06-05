903406fcc309a11a4818b578ba72a0398eba0ddd
Nathaniel Dsouza <nate@kidzideaz.xyz>
2021-06-05 16:04:36 -0400

    Make `useTracker` a named export from the package

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import Tracker from './Tracker.js'
 Object.assign(Meteor, User);
 
 const {
-    withTracker, Mongo, packageInterface, ReactiveDict
+    useTracker, withTracker, Mongo, packageInterface, ReactiveDict
 } = Meteor;
 
-export { Accounts, withTracker, Mongo, packageInterface, ReactiveDict, Tracker };
+export { useTracker, Accounts, withTracker, Mongo, packageInterface, ReactiveDict, Tracker };
 export default Meteor;
