7075dc9b80949e09bcd78165a9431116fd674dbd
jankapunkt <jkuester@uni-bremen.de>
2024-02-14 10:22:36 +0100

    fix: strictly respect autoConnect flag

--- test/src/Meteor.tests.js
+++ test/src/Meteor.tests.js
@@ -4,6 +4,10 @@ import { awaitDisconnected, stub, restoreAll } from '../testHelpers';
 import DDP from '../../lib/ddp';
 
 describe('Meteor - integration', function () {
+  afterEach(() => {
+    restoreAll();
+  });
+
   it('uses the default async storage if none is defined', function () {
     const fallback =
       require('@react-native-async-storage/async-storage').default;
@@ -13,9 +17,37 @@ describe('Meteor - integration', function () {
 
   describe(Meteor.connect.name, () => {
     before(awaitDisconnected);
-    afterEach(() => {
-      restoreAll();
+
+    it('requires manual connect if autoConnect is  set to false', function (done) {
+      this.timeout(3000);
+      expect(Meteor.getData().ddp.status).to.equal('disconnected');
+      stub(DDP.prototype, 'on', () => {});
+      let connectCalled = 0;
+      stub(DDP.prototype, 'connect', () => {
+        done(new Error('should not automatically call connect'));
+      });
+
+      const AsyncStorage = {
+        getItem: async () => {},
+        setItem: async () => {},
+        removeItem: async () => {},
+      };
+
+      const endpoint = `ws://localhost:3000/websocket`;
+      Meteor.connect(endpoint, {
+        AsyncStorage,
+        NetInfo: null,
+        autoConnect: false,
+      });
+
+      // let's wait some time to make sure no internals
+      // unintentionally call ddp.connect before we do
+      setTimeout(() => {
+        expect(Meteor.getData().ddp.status).to.equal('disconnected');
+        done();
+      }, 2900);
     });
+
     it('allows to bypass NetInfo', (done) => {
       stub(DDP.prototype, 'on', () => {});
       stub(DDP.prototype, 'connect', done);
@@ -30,12 +62,20 @@ describe('Meteor - integration', function () {
       Meteor.connect(endpoint, {
         AsyncStorage,
         NetInfo: null,
-        autoConnect: false,
       });
     });
     it('allows to pass a custom configured NetInfo', (done) => {
       stub(DDP.prototype, 'on', () => {});
-      stub(DDP.prototype, 'connect', done);
+
+      let connectCalled = 0;
+      stub(DDP.prototype, 'connect', () => {
+        connectCalled++;
+        if (connectCalled > 1) {
+          done(new Error('should not call more than once!'));
+        } else {
+          done();
+        }
+      });
 
       const AsyncStorage = {
         getItem: async () => {},
@@ -45,7 +85,9 @@ describe('Meteor - integration', function () {
 
       const NetInfo = {
         addEventListener: (cb) => {
-          setTimeout(() => cb({ isConnected: true }), 300);
+          setTimeout(() => {
+            cb({ isConnected: true });
+          }, 0);
         },
       };
 
@@ -53,7 +95,6 @@ describe('Meteor - integration', function () {
       Meteor.connect(endpoint, {
         AsyncStorage,
         NetInfo,
-        autoConnect: false,
         autoReconnect: true,
       });
     });
